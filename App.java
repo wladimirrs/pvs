a6a70d9e31bac92c94dec491c3ae1859040090af
wladimirrs <185645045+wladimirrs@users.noreply.github.com>
2026-06-02 11:13:10 +0200

    CRUD ohne R noch

--- App.java
+++ App.java
@@ -5,7 +5,14 @@ public class App {
 
         Mitarbeiter m1 = new Mitarbeiter(8, "230230",  "Peter", "Peter", "Petrestraße", "10", 1, 1);
         Statements s1 = new Statements();
-        s1.einfuegen(m1);
+        /*s1.einfuegen(m1); // funktioniert*/
+        /*s1.loeschen(8); // funktioniert*/
+
+        /*m1.setNachname("Rudolf"); // funktioniert
+        m1.setVorname("Rudolf");
+        s1.aendern(m1);*/
+
+
 
 
 
--- Statements.java
+++ Statements.java
@@ -4,14 +4,14 @@ import java.sql.*;
 
 public class Statements {
 
-    private final String url = "\"jdbc:mysql://127.0.0.1:3306/pvs";
+    private final String url = "jdbc:mysql://127.0.0.1:3306/pvs";
     private final String user = "root";
     private final String password = "";
 
 
 
 
-    public void einfuegen(Mitarbeiter m) {
+    public void einfuegen(Mitarbeiter m) {  // erstellten Mitarbeiter m in DB einfügen
         String sql = "INSERT INTO mitarbeiter (id, personalnummer, nachname, vorname, strasse, hausnummer, ort, ressort) VALUES" +
                 " (?, ?, ?, ?, ?, ?, ?, ?)";
         try (Connection conn = DriverManager.getConnection(url, user, password);
@@ -24,11 +24,63 @@ public class Statements {
             ps.setString(6, m.getHausnr());
             ps.setInt(7, m.getOrt());
             ps.setInt(8, m.getRessort());
+            ps.executeUpdate();
+            System.out.println("Datensatz erfolgreich eingefügt");
         } catch
         (SQLException e) {
             System.out.println("Datensatz nicht eingefügt: " + e.getMessage());
         }
     }
+
+
+
+
+
+
+
+    public void loeschen (int id) {
+        String sql = "DELETE FROM mitarbeiter WHERE id = ?";
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            System.out.println("Datensatz gelöscht.");
+        } catch (SQLException e) {
+            System.out.println("Datensatz nicht gelöscht: " + e.getMessage());
+        }
+    }
+
+
+
+    public void aendern (Mitarbeiter m) {
+        String sql = "UPDATE mitarbeiter SET personalnummer = ?, nachname = ?, vorname = ?, strasse = ?, hausnummer = ?, ort = ?, ressort = ? WHERE id = ?";
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, m.getPersnr());
+            ps.setString(2, m.getNachname());
+            ps.setString(3, m.getVorname());
+            ps.setString(4, m.getStrasse());
+            ps.setString(5, m.getHausnr());
+            ps.setInt(6, m.getOrt());
+            ps.setInt(7, m.getRessort());
+            ps.setInt(8, m.getId());
+            ps.executeUpdate();
+            System.out.println("Datensatz erfolgreich aktualisiert.");
+        } catch (SQLException e) {
+            System.out.println("Datensatz nicht aktualisiert: " + e.getMessage());
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
